b25e34c88576fc4cd79bc6a26649a1475af11fcd
Jon Williams <jrwilliams100@hotmail.co.uk>
2020-12-20 22:59:57 +0000

    Fixed html main layout columns

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 'use strict';
 
 let secretNumber = Math.floor(Math.random() * 20) + 1;
-// Reveals secret number
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
+    const guess = Number(document.querySelector('.guess').value);
+    
+    if (!guess) {
+        document.querySelector('.message').textContent = 'No Number';
+    }
+    
+    // Reveals secret number
+    document.querySelector('.number').textContent = secretNumber;
 });
