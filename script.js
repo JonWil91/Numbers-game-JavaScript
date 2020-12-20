b4dfb09ef1c714a5b6e6904bce0daa81d29d9bf7
Jon Williams <jrwilliams100@hotmail.co.uk>
2020-12-20 22:18:27 +0000

    Added JS random number generator

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 'use strict';
 
+let secretNumber = Math.floor(Math.random() * 20) + 1;
+// Reveals secret number
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
