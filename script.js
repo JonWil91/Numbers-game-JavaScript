88f09aa4afa18bf163787cb436c2ca2e3a5b36fb
Jon Williams <jrwilliams100@hotmail.co.uk>
2020-12-21 10:17:28 +0000

    Added game logic: number is higher/lower/correct

--- script.js
+++ script.js
@@ -4,11 +4,27 @@ let secretNumber = Math.floor(Math.random() * 20) + 1;
 
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
-    
+
+    // No number input
     if (!guess) {
         document.querySelector('.message').textContent = 'No Number';
     }
-    
-    // Reveals secret number
-    document.querySelector('.number').textContent = secretNumber;
+
+    // When player wins
+    else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = 'Correct Number! 💥';
+
+        // Reveals secret number
+        document.querySelector('.number').textContent = secretNumber;
+    }
+
+    // When guess is too high
+    else if (guess > secretNumber) {
+        document.querySelector('.message').textContent = 'Too High';
+    }
+
+    else if (guess < secretNumber) {
+        document.querySelector('.message').textContent = 'Too Low';
+    }
+
 });
